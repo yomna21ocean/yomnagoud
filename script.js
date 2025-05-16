bfd4778532921146c21db626f01836400a43d4b7
yomna21ocean <121353913+yomna21ocean@users.noreply.github.com>
2025-05-17 02:50:15 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, { threshold: 0.1 });
+
+sections.forEach(section => {
+  observer.observe(section);
+});
